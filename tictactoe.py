067e1ceecc1a0bef38ef815cad989d7426267a7b
jere99 <jrdegreeff@mxschool.edu>
2019-01-04 23:30:50 -0500

    Add basic draw loop

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,28 @@
 import game
+import graphics
+
+LENGTH = 3
+DIMENSIONALITY = 2
+
+MARGIN_CONSTANT = 0.9
 
 def setup():
     this.surface.setTitle("Tic-Tac-Toe")
-    size(this.displayWidth, this.displayHeight)
+    fullScreen()
+
+    global SIZE, CELL_SIZE, X_MARGIN, Y_MARGIN
+    SIZE = min(width, height) * MARGIN_CONSTANT
+    CELL_SIZE = SIZE / LENGTH
+    X_MARGIN = (width - SIZE) * 0.5
+    Y_MARGIN = (height - SIZE) * 0.5
+
+    global tictactoe
+    tictactoe = game.Game(LENGTH, DIMENSIONALITY)
 
 def draw():
     background(51)
-    noLoop()
+    stroke(255)
+    strokeWeight(4)
+    for i in range(1, LENGTH):
+        line(X_MARGIN, Y_MARGIN + i * CELL_SIZE, width - X_MARGIN, Y_MARGIN + i * CELL_SIZE)
+        line(X_MARGIN + i * CELL_SIZE, Y_MARGIN, X_MARGIN + i * CELL_SIZE, height - Y_MARGIN)
